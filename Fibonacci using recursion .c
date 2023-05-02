c4aa87deb472043c4ec26cfd7f4fc057eebea34d
Pratush Singh <120071839+Pratyush8055@users.noreply.github.com>
2023-05-02 10:24:54 +0530

    Update Fibonacci using recursion .c

--- Fibonacci using recursion .c
+++ Fibonacci using recursion .c
@@ -12,7 +12,7 @@ int main() {
     int n;
     printf("\nEnter a number ");
     scanf("%d",&n);
-    if(n<0)
+    if(n>0)
     printf("\nfactorial not possible");
     else
     for(int i=0;i<n;i++)
